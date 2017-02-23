5616a7cc898ad905caa52630dca55a71df22a712
Art Manalili <artmanalili@gmail.com>
2017-02-23 15:23:07 +0800

    Fixed Sign Up and review Details flow. No Recovery Password yet after finishing the sign up process

--- src/routes.web.js
+++ src/routes.web.js
@@ -5,6 +5,7 @@ import Container from './modules/Container.web'
 import Signup from './modules/Signup/SignupContainer.web'
 import Login from './modules/Login/Login.web'
 import Home from './modules/Home/Home.web'
+import ReviewDetails from './modules/ReviewDetails/ReviewDetails.web'
 
 export default class RouterComponent extends Component {
 
@@ -15,6 +16,7 @@ export default class RouterComponent extends Component {
           <IndexRedirect to='/login' />
           <Route path='/login' component={Login} />
           <Route path='/signup' component={Signup} />
+          <Route path='/review' component={ReviewDetails} />
           <Route path='/home' component={Home} />
         </Route>
       </Router>
